d533220c4640a02c82c21ada620dd8e51927a692
Sipatha <186062+sipatha@users.noreply.github.com>
2020-09-25 14:20:37 +0200

    - create and publish docker image 024-debugging

--- src/test/java/com/vambita/work/status/StatusApplicationTests.java
+++ src/test/java/com/vambita/work/status/StatusApplicationTests.java
@@ -1,13 +1,22 @@
 package com.vambita.work.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.servlet.ServletContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StatusApplicationTests {
 
+	@Autowired
+	private ServletContext context;
+
 	@Test
 	void contextLoads() {
+		assertEquals("MockServletContext", context.getServerInfo());
 	}
 
 }
